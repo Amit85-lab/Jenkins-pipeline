ad1b98f66678ca0697b00bf5f3c90274464cfded
Amit85-lab <amit.ghosh85@gmail.com>
2026-04-27 14:42:55 +0530

    Update callingfunction.groovy

--- vars/callingfunction.groovy
+++ vars/callingfunction.groovy
@@ -1,9 +1,4 @@
-def checkout() {
-    echo "Hello, This is ${name} from devops team"
-}
+
 def build() {
     echo "coding, This is ${name} from devops team"
-}
-def deploy() {
-    echo "deploy, This is ${name} from devops team"
 }
